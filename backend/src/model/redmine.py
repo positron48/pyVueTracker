a02f3f8c7d69b6124760a3146fbd7f60e330d50a
Dmitry Averin <averin@intaro.email>
2018-12-25 12:30:20 +0300

    refactor(redmine)

--- backend/src/model/redmine.py
+++ backend/src/model/redmine.py
@@ -1,15 +1,29 @@
+from redminelib import Redmine as RedmineLib
+from redminelib.exceptions import AuthError
+
+
 class Redmine(object):
     def __init__(self, url, token=None, login=None,
                  password=None):
-        from redminelib import Redmine
-        self.api = None  # type: Redmine
+        self.api = None  # type: RedmineLib
         if token is None:
-            self.api = Redmine(url, username=login, password=password)
+            self.api = RedmineLib(url, username=login, password=password)
         else:
-            self.api = Redmine(url, key=token)
+            self.api = RedmineLib(url, key=token)
+
+    def is_auth(self):
+        try:
+            auth = self.api.auth()
+        except AuthError:
+            auth = None
+        return auth is not None
 
     def get_all_projects(self):
         return self.api.project.all()
 
     def get_all_tasks(self):
         return self.api.issue.all()
+
+    def get_api_key(self):
+        if self.is_auth():
+            return self.api.auth().api_key
