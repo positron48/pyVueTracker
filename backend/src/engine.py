53c5982c7866c7c822ed3a00b347b3aa4fc4669c
Dmitry Averin <averin@intaro.email>
2019-01-21 16:13:25 +0300

    фикс связей спроектами с id=0 при удалении через фронт

--- backend/src/engine.py
+++ backend/src/engine.py
@@ -219,6 +219,13 @@ class Engine:
             .filter(TrackerProjectLink.tracker_id == tracker_id) \
             .filter(TrackerProjectLink.project_id == project_id).first()
 
+        # для id=0 удаляем связь, если она есть
+        if tracker_project_id == 0:
+            if tracker_project is not None:
+                db.session.delete(tracker_project)
+                db.session.commit()
+            return True
+
         if tracker_project is not None:
             tracker_project.external_project_id = tracker_project_id
             tracker_project.external_project_title = tracker_project_title
