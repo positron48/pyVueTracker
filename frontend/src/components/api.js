43c7a307400afbc455d1de728792d186c6781c7e
Filatov Anton <positron48@gmail.com>
2019-08-30 18:33:38 +0300

    bug with select date

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -3,7 +3,7 @@ import {isLogin, logout, getToken} from './auth.js'
 import {urlEncode, formatDate} from './helpers.js'
 
 const HTTP = axios.create({
-  baseURL: 'https://time.skillum.ru',
+  baseURL: 'http://localhost:5000',
   headers: {
     'Content-type': 'application/x-www-form-urlencoded'
   }
